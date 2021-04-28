00fe7a028273fa88ea7f72b4631cc9cf72918657
Tric Rullan <tricrullan@gmail.com>
2021-04-28 23:15:26 +0800

    Initial commit - basic project setup

---
+++ AppChallenge/AppChallenge/Features/Login/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  AppChallenge
+//
+//  Created by Tric Rullan on 4/28/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ AppChallenge/Pods/Target Support Files/Pods-AppChallenge-AppChallengeUITests/Pods-AppChallenge-AppChallengeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppChallenge_AppChallengeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AppChallenge_AppChallengeUITests
+@end
